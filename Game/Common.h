737de44779460b5135747ce81d122e3fd7dbb66a
scorpiontron <meurisseperezb@gmail.com>
2026-03-17 14:20:21 +0100

    merge common fonction

--- Game/Common.h
+++ Game/Common.h
@@ -10,7 +10,6 @@
 
 #include "Backup.h"
 #include "EntityManager.h"
-#include "CommonFonction.h"
 
 #define GAME_SCALE 1
 #define SCREEN_WIDTH 1024
@@ -18,4 +17,56 @@
 #define BPP 32
 
 # define DEV_MODE 1
+
+typedef struct Animation
+{
+	sfIntRect rectActualy;
+	sfBool isLooping;
+	int frameCount;
+	float frameDuration;
+	float timeActualy;
+}Animation;
+
+typedef enum  Direction
+{
+	DOWN,
+	LEFT,
+	RIGHT,
+	UP,
+}Direction;
+
+
+
+typedef struct Element
+{
+	void* value;
+	struct Element* next;
+}Element;
+
+typedef struct List
+{
+	Element* first;
+}List;
+
+
+
+int GetRandomInRange(int _min, int _max);
+float GetAngleToVector(sfVector2f _vector);
+float RadToDeg(float _angleRad);
+float DegToRad(float _angleDeg);
+void UpdateText(sfText* _text, char* _format, char* _string, int _value);
+sfBool IsColidingPionHitbox(sfFloatRect* _hitbox, sfVector2f _position);
+sfBool UpdateAnimationAndGiveIfStop(sfSprite* _sprite, Animation* const _animation, float _dt);
+
+
+
+List* CreateList(void);
+void RemoveList(List* _list);
+unsigned int GetListSize(List* _list);
+
+Element* CreateElement(void* _value);
+Element* GetElement(List* _list, unsigned int _index);
+void InsertElement(List* _list, Element* _element, unsigned int _index);
+void RemoveElement(List* _list, unsigned int _index);
+
 #endif // !COMMON_H
