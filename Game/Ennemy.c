1667c12f0e28334eb0ee94cc968cf9e475eb9444
PierrePagliazzo <pagliazzopierre@gmail.com>
2026-03-17 10:30:30 +0100

    Mise a jour Ennemy

--- Game/Ennemy.c
+++ Game/Ennemy.c
@@ -4,3 +4,10 @@ void LoadEnnemy(void)
 {
 
 }
+
+Ennemy* CreateRandomEnnemy(void)
+{
+	Ennemy ennemy = { 0 };
+	ennemy.typeEnnemy = 0;
+	return NULL;
+}
--- Game/Ennemy.h
+++ Game/Ennemy.h
@@ -3,13 +3,44 @@
 
 #include "Common.h"
 
-typedef struct Ennemy
+enum Vitesse
 {
-	int Life;
+	TRES_LENT = 5,
+	LENT = 20,
+	NORMAL = 50
 };
 
+enum TypeEnnemy
+{
+	BALLOM,
+	ONIL,
+	DALL,
+	DENKYUN,
+	KONDORIA,
+	TOTAL_Type_Ennemy
+};
+
+typedef struct Personality
+{
+	int aggressiveness;
+	int chaos;
+}Personality;
+
+typedef struct Ennemy
+{
+	sfSprite* sprite;
+	int life;
+	enum TypeEnnemy typeEnnemy;
+	int points;
+	enum Vitesse vitesse;
+	_Bool passeMuraille;
+	Personality personality;
+}Ennemy;
 
 void LoadEnnemy(void);
+Ennemy* CreateRandomEnnemy(void);
+Ennemy* CreateSelectEnnemy(enum TypeEnnemy _type);
+
 
 
 //sfRectangleShape_getGlobalBounds;
