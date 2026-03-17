4b955b1b9416bbf72b41abef27a41fc780f9d173
akiramen31 <akiraaillet@gmail.com>
2026-03-17 10:41:33 +0100

    Create Player.h

---
+++ Game/Player.h
@@ -0,0 +1,33 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Common.h"
+#include "Animation.h"
+
+enum struct Direction
+{
+	DOWN,
+	LEFT,
+	RIGHT,
+	UP,
+} ;
+
+enum struct State
+{
+	IDLE = 0,
+	WALKING = 4
+} ;
+
+typedef struct Player
+{
+	Direction direction;
+	State state;
+
+	
+}Player;
+
+void PlayerLoad(void);
+void PlayerUpdate(float _dt);
+
+void PlayerMove(float _dt);
+#endif
