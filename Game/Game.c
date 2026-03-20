32c4dc0ea7a12b9da27939cf2a1c4814bc708b1e
Pyxal42 <pixalnotfound@gmail.com>
2026-03-20 10:14:22 +0100

    Fixed starting values

--- Game/Game.c
+++ Game/Game.c
@@ -9,6 +9,13 @@ float timer;
 
 void LoadGame(void)
 {
+	SetIntToSave(SCORE, 0);
+	SetIntToSave(BOMB, 1);
+	SetIntToSave(SPEED, 1);
+	SetIntToSave(ENNEMY_COUNT, 1);
+	SetIntToSave(FIRE, 1);
+	SetIntToSave(LIFE, 4);
+
 	game = (Game){ 0 };
 	LoadBackground(GetAsset("Assets/Sprites/Map/Background.png"), 4.f);
 	CreateSprite(GetAsset("Assets/Sprites/Map/Foreground.png"), (sfVector2f) { 0 }, 4.f, 2.f);
