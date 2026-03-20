2a117a63ecc5645b1248a47528dd4060645d8c9c
Pyxal42 <pixalnotfound@gmail.com>
2026-03-20 09:34:42 +0100

    added part of the time again

--- Game/HUD.c
+++ Game/HUD.c
@@ -9,35 +9,32 @@ sfSprite* time[5];
 
 void LoadHUD()
 {
+	//Score
 	CreateSprite(GetAsset("Assets/Sprites/HUD/HUD.png"), (sfVector2f) { 0 }, 4.f, 10);
 	for (int i = 0; i < 6; i++)
 	{
 		score[i] = CreateSprite(GetAsset("Assets/Sprites/HUD/Numbers.png"), (sfVector2f) { 776+(36 * i), 16 }, 4.f, 0);
 		sfSprite_setTextureRect(score[i], (sfIntRect) { 0, 0, 10, 12 });
 	}
+	//PowerUps
 	for (int i = 0; i < 3; i++)
 	{
 		powerUps[i] = CreateSprite(GetAsset("Assets/Sprites/HUD/Numbers.png"), (sfVector2f) { 152 + ( 208 * i), 832 }, 4.f, 0);
 		sfSprite_setTextureRect(powerUps[i], (sfIntRect) { 10, 0, 10, 12 });
 	}
+	//Life
 	playerLife = CreateSprite(GetAsset("Assets/Sprites/HUD/Numbers.png"), (sfVector2f) { 136, 30 }, 3.f, 0);
 	sfSprite_setTextureRect(playerLife, (sfIntRect) { 10, 0, 10, 12 });
-	for (int i = 0; i < 3; i++)
+	//Time
+	for (int i = 0; i <2 ; i++)
 	{
-		time[i] = CreateSprite(GetAsset("Assets/Sprites/HUD/Numbers.png"), (sfVector2f) { 384 + (208 * i), 832 }, 4.f, 0);
-		if (i < 3)
-		{
-
-		}
-		else if (i == 3)
-		{
-			sfSprite_setTextureRect(powerUps[i], (sfIntRect) { 100, 0, 10, 12 });
-		}
-		else
-		{
-			sfSprite_setTextureRect(powerUps[i], (sfIntRect) { 0, 0, 10, 12 });
-		}
+		time[i] = CreateSprite(GetAsset("Assets/Sprites/HUD/Numbers.png"), (sfVector2f) { 384 + (36 * i), 16 }, 4.f, 0);
+		sfSprite_setTextureRect(time[i], (sfIntRect) { 0, 0, 10, 12 });
+		time[3 + i] = CreateSprite(GetAsset("Assets/Sprites/HUD/Numbers.png"), (sfVector2f) { 476 + (36* i), 16 }, 4.f, 0);
+		sfSprite_setTextureRect(time[3+i], (sfIntRect) { 0, 0, 10, 12 });
 	}
+	time[2] = CreateSprite(GetAsset("Assets/Sprites/HUD/Numbers.png"), (sfVector2f) { 448, 16 }, 4.f, 0);
+	sfSprite_setTextureRect(time[2], (sfIntRect) { 100, 0, 10, 12 });
 }
 
 void UpdateHUD(float _dt, float _timer)
@@ -53,6 +50,7 @@ void UpdateHUD(float _dt, float _timer)
 	for (int i = 0; i < 3; i++)
 	{
 		int number = GetIntToSave(2+i);
+		printf("%d, %d\n", number, i);
 		sfSprite_setTextureRect(powerUps[i], (sfIntRect){10 * number, 0, 10, 12});
 	}
 	//PlayerLife
